44e9571516ee96f93b1612b8e84df74adfd7953c
Terry Velivasakis <terry.velivasakis@sofwerx.org>
2024-11-13 17:02:23 -0500

    end of the day

--- flask/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    # Placeholder for future MongoDB URI or other configurations
-    SECRET_KEY = "your_secret_key"
--- flask/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user_routes import user_bp
-
-def register_blueprints(app):
-    app.register_blueprint(user_bp, url_prefix="/user")
